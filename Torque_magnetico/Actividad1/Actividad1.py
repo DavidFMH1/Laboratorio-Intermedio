980d19f7a061921b3b3740fd5dd7ef003ac57b79
DavidFMH1 <david_monroyhernandez@hotmail.com>
2025-04-22 22:19:21 -0500

    act1 torque

--- Torque_magnetico/Actividad1/Actividad1.py
+++ Torque_magnetico/Actividad1/Actividad1.py
@@ -26,6 +26,9 @@ def Weighted_least_squares(x,y,dy):
     
     return c, sigc, m, sigm
 
+def linealfunc(x,a,b):
+    return a*x + b
+
 def read_data(path):
     
     col1 = np.array([])
@@ -51,5 +54,23 @@ Rm = Rad/100
 
 rmg = Rm*0.0015*9.78
 
-plt.scatter(MField,rmg)
+sigrmg = np.sqrt(5.38e-13+(Rm*0.0015*0.0001)**2)
+sigB = np.full(len(MField),6.8e-5)
+
+c, sc, m, sm = Weighted_least_squares(MField,rmg,sigrmg)
+
+_x = np.linspace(np.min(MField)-6.8e-5,np.max(MField)+6.8e-5,2)
+_y = linealfunc(_x,m,c)
+
+
+fig, axs = plt.subplots(2, 1, figsize=(7,5.5), gridspec_kw={'height_ratios': [3, 1], 'hspace': 0, 'wspace': 0.15})
+
+fig.suptitle('Torque mecánico en función del campo magnético', fontsize=12)
+
+axs[0].errorbar(MField, rmg, yerr=sigrmg, xerr=sigB, fmt='o', label='Datos',color='blue', capsize=5, elinewidth=1.5, markersize=6)
+axs[0].plot(_x,_y,color='red')
+
+for row in axs:
+    row.grid(visible=True, linestyle="--", linewidth=0.7, alpha=0.7)
+
 plt.show()
