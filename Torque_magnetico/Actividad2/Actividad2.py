913b75438dbd1ea573d5394ecbb7762b5082585f
DavidFMH1 <david_monroyhernandez@hotmail.com>
2025-04-24 17:25:19 -0500

    actividad 2 completa

--- Torque_magnetico/Actividad2/Actividad2.py
+++ Torque_magnetico/Actividad2/Actividad2.py
@@ -59,24 +59,40 @@ MfieldN = 1/Mfield
 per2 = (per/5)**2
 
 sigBn = (1/Mfield**2)*6.8e-5
+sigper2 = 2*(per/5)*0.02
 
 c, sigc, m, sigm = Weighted_least_squares(per2, MfieldN, sigBn) 
 
 mu = m/NF
 sigmu = np.sqrt((8*(np.pi**2)*(R**2)*m*sM)**2 + (16*(np.pi**2)*R*m*sR)**2 + (8*(np.pi**2)*(R**2)*M*sigm)**2)
-print(mu,sigmu)
 
-_x = np.linspace(np.min(per2),(np.max(per2)))
+print(f'De esta actividad se puede deducir un valor de mu {mu:.2f} mas o menos {sigmu:.2f}')
+
+_x = np.linspace(np.min(per2)-sigper2[0],(np.max(per2))+3*sigper2[0])
 _y = linealfunc(_x,m,c)
+
+#residuales
+
+res = (MfieldN - linealfunc(per2,m,c))/sigBn
 
 fig, axs = plt.subplots(2, 1, figsize=(7,5.5), gridspec_kw={'height_ratios': [3, 1], 'hspace': 0, 'wspace': 0.15})
 
+mt = fr'$m = {m:.0f} \pm {sigm:.0f}$'
+ct = fr'$b = {c:.0f} \pm {sigc:.0f}$'
+
 fig.suptitle('Inverso del campo en función del periodo al cuadrado', fontsize=12)
 
-axs[0].errorbar(per2, MfieldN, yerr=sigBn, fmt='o', label='Datos',color='blue', capsize=5, elinewidth=1.5, markersize=6)
-axs[0].plot(_x,_y,color='red')
+axs[0].errorbar(per2, MfieldN, yerr=sigBn, xerr=sigper2, fmt='o', label='Datos',color='black', capsize=3, elinewidth=1.5, markersize=6)
+axs[0].plot(_x,_y,color='red', label=f'Regresión Lineal \n {mt} \n {ct}')
+axs[0].set_ylabel(r'$\frac{1}{B}$ $(T^{-1})$', fontsize=14)
+axs[0].legend()
+
+axs[1].scatter(per2, res, color='black', marker='x')
+axs[1].axhline(0, color='black', linestyle='--')
+axs[1].set_ylabel('Residuales \n normalizados', fontsize=12)
+axs[1].set_xlabel(r'$T^{2}$ $(s^{2})$', fontsize=12)
 
 for row in axs:
     row.grid(visible=True, linestyle="--", linewidth=0.7, alpha=0.7)
 
-plt.show()
+plt.savefig(r'Torque_magnetico\\Actividad2\\campontvsperiodo22.png', format='png', dpi=300)
